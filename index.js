b6103ec609e0c06b5231f6747d7acaf12d311ed7
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-09-29 08:58:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = 3001
 const bodyParser = require('body-parser')
 const db = require('./connection.js')
 const response = require('./response.js')
