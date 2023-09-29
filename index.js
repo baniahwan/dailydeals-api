050e6570ba38ecebf68c1a7035f7b806246aa01e
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-09-29 09:01:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const response = require('./response.js')
 
 
 // Middleware untuk mengizinkan CORS (Cross-Origin Resource Sharing)
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.Header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.Header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.use(bodyParser.json())
 
