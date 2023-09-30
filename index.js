e102338871455079a75d5d24e9508232f21a2d8d
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-09-30 14:55:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
-  response(200, "Ini data", "Ini adalah API daily deals. (backend capstone projek section palembang group 3", res)
+  response(200, "Ini data", `Ini adalah API daily deals. (backend capstone projek section palembang group 3.
+    Gunakan /menu untuk get all menu.
+    Gunakan /menu/burger untuk get menu by ketegori burger
+    Gunakan /menu/sandwich untuk get menu by ketegori sandwich
+    Gunakan /menu/dessertAndDrink untuk get menu by ketegori dessert & drink
+    Gunakan /menu/sides untuk get menu by ketegori sides
+    Gunakan /register untuk register (post)
+    Gunakan /login untuk login (post)`, res)
 })
 
 // Get all menu
