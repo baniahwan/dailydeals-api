03ec15df5457c5b2b229a4b8d02b911cf74f9fc4
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-10-05 23:58:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -136,11 +136,11 @@ app.post('/keranjang', (req, res) => {
   });
 });
 
-// UNTUK MENAMPILKAN DATA DARI TABEL keranjang
+// UNTUK MENAMPILKAN DATA MENU DARI TABEL keranjang DENGAN ID MENU
 app.get('/keranjang/:id_menu', (req, res) => {
   const id_menu = req.params.id_menu
-  const sql = `SELECT menu.nama, menu.gambar, menu.harga, keranjang.jumlah_item, keranjang.total_harga, keranjang.id 
-               FROM keranjang JOIN menu ON keranjang.id_menu = menu.id 
+  const sql = `SELECT menu.nama, menu.gambar, menu.harga, keranjang.jumlah_item, keranjang.total_harga, keranjang.id_keranjang 
+               FROM keranjang JOIN menu ON keranjang.id_menu = menu.id_menu 
                WHERE keranjang.id_menu = ${id_menu}`
   db.query(sql, (err, fields) => {
     if (err) throw err
@@ -151,9 +151,9 @@ app.get('/keranjang/:id_menu', (req, res) => {
 // UNTUK MENAMPILKAN SEMUA DATA DARI TABEL KERANJANG DENGAN ID USER
 app.get('/keranjang/user/:id_user', (req, res) => {
   const id_user = req.params.id_user;
-  const sql = `SELECT menu.nama, menu.gambar, menu.harga, keranjang.jumlah_item, keranjang.total_harga 
+  const sql = `SELECT menu.id_menu, menu.nama, menu.gambar, menu.harga, keranjang.jumlah_item, keranjang.total_harga 
                FROM keranjang 
-               JOIN menu ON keranjang.id_menu = menu.id 
+               JOIN menu ON keranjang.id_menu = menu.id_menu
                WHERE keranjang.id_user = ${id_user}`;
   db.query(sql, (err, fields) => {
     if (err) throw err;
