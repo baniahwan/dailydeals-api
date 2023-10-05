0006ad3cdd9d1c27c104e52ff8bbfaf972b32623
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-10-05 23:07:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.post('/keranjang', (req, res) => {
 // UNTUK MENAMPILKAN DATA DARI TABEL keranjang
 app.get('/keranjang/:id_menu', (req, res) => {
   const id_menu = req.params.id_menu
-  const sql = `SELECT menu.nama, menu.gambar, menu.harga, keranjang.jumlah_item, keranjang.total_harga 
+  const sql = `SELECT menu.nama, menu.gambar, menu.harga, keranjang.jumlah_item, keranjang.total_harga, keranjang.id 
                FROM keranjang JOIN menu ON keranjang.id_menu = menu.id 
                WHERE keranjang.id_menu = ${id_menu}`
   db.query(sql, (err, fields) => {
@@ -161,7 +161,7 @@ app.get('/keranjang/user/:id_user', (req, res) => {
   });
 });
 
-// UNTUK MENGHAPUS DATA MENU DARI KERANJANG
+// UNTUK MENGHAPUS DATA MENU DARI KERANJANG (dengan id keranjang)
 app.delete('/deleteitemcart', (req, res) => {
   const { id } = req.body
   const sql = `DELETE FROM keranjang WHERE id=${id}`
