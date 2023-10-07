45d88333401a739a936b971d1476781cdd05866a
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-10-07 13:21:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const crypto = require('crypto')
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5501');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
