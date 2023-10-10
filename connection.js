0c73fce698044fb04ea5329697aab6f649e9b05d
Nur Sya'bani Ahwan <nursyabaniahwan@gmail.com>
2023-10-10 14:22:19 +0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "b9b9aottiooh6egdqsku-mysql.services.clever-cloud.com",
     user: "uqpbwhwstmid9izt",
     password: "A1Q3wKffeK3viigreLip",
     database: "b9b9aottiooh6egdqsku",
-    port: 3306
+    port: 3306,
+    connectionLimit: 10
 })
 
 module.exports = db
